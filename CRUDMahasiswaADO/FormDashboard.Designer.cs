247a454a88b26496da62a7c8772d5613f2fc62ef
Egin Pardewan <eginpardewan@gmail.com>
2026-06-22 19:13:10 +0700

    membuat desaign grafik

---
+++ CRUDMahasiswaADO/FormDashboard.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CRUDMahasiswaADO
+{
+    partial class FormDashboard
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblJudul = new System.Windows.Forms.Label();
+            this.lblTahun = new System.Windows.Forms.Label();
+            this.cmbTahun = new System.Windows.Forms.ComboBox();
+            this.lblTipe = new System.Windows.Forms.Label();
+            this.cmbTipe = new System.Windows.Forms.ComboBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnDataMahasiswa = new System.Windows.Forms.Button();
+            this.chartProdi = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartProdi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblJudul
+            // 
+            this.lblJudul.AutoSize = true;
+            this.lblJudul.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblJudul.ForeColor = System.Drawing.Color.DarkBlue;
+            this.lblJudul.Location = new System.Drawing.Point(250, 20);
+            this.lblJudul.Name = "lblJudul";
+            this.lblJudul.Size = new System.Drawing.Size(356, 37);
+            this.lblJudul.TabIndex = 0;
+            this.lblJudul.Text = "REKAP DATA MAHASISWA";
+            // 
+            // lblTahun
+            // 
+            this.lblTahun.AutoSize = true;
+            this.lblTahun.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblTahun.Location = new System.Drawing.Point(30, 80);
+            this.lblTahun.Name = "lblTahun";
+            this.lblTahun.Size = new System.Drawing.Size(114, 23);
+            this.lblTahun.TabIndex = 1;
+            this.lblTahun.Text = "Tahun Masuk:";
+            // 
+            // cmbTahun
+            // 
+            this.cmbTahun.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTahun.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmbTahun.Location = new System.Drawing.Point(140, 77);
+            this.cmbTahun.Name = "cmbTahun";
+            this.cmbTahun.Size = new System.Drawing.Size(100, 31);
+            this.cmbTahun.TabIndex = 2;
+            // 
+            // lblTipe
+            // 
+            this.lblTipe.AutoSize = true;
+            this.lblTipe.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblTipe.Location = new System.Drawing.Point(587, 80);
+            this.lblTipe.Name = "lblTipe";
+            this.lblTipe.Size = new System.Drawing.Size(95, 23);
+            this.lblTipe.TabIndex = 3;
+            this.lblTipe.Text = "Tipe Grafik:";
+            // 
+            // cmbTipe
+            // 
+            this.cmbTipe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipe.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmbTipe.Location = new System.Drawing.Point(688, 77);
+            this.cmbTipe.Name = "cmbTipe";
+            this.cmbTipe.Size = new System.Drawing.Size(100, 31);
+            this.cmbTipe.TabIndex = 4;
+            this.cmbTipe.SelectedIndexChanged += new System.EventHandler(this.cmbTipe_SelectedIndexChanged);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnLoad.ForeColor = System.Drawing.Color.Transparent;
+            this.btnLoad.Location = new System.Drawing.Point(257, 77);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(80, 30);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Red;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(343, 77);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(80, 30);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDataMahasiswa
+            // 
+            this.btnDataMahasiswa.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnDataMahasiswa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDataMahasiswa.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnDataMahasiswa.ForeColor = System.Drawing.Color.Black;
+            this.btnDataMahasiswa.Location = new System.Drawing.Point(600, 442);
+            this.btnDataMahasiswa.Name = "btnDataMahasiswa";
+            this.btnDataMahasiswa.Size = new System.Drawing.Size(154, 30);
+            this.btnDataMahasiswa.TabIndex = 7;
+            this.btnDataMahasiswa.Text = "Data Mahasiswa";
+            this.btnDataMahasiswa.UseVisualStyleBackColor = false;
+            this.btnDataMahasiswa.Click += new System.EventHandler(this.btnDataMahasiswa_Click);
+            // 
+            // chartProdi
+            // 
+            chartArea1.Name = "MainArea";
+            this.chartProdi.ChartAreas.Add(chartArea1);
+            legend1.Name = "MainLegend";
+            this.chartProdi.Legends.Add(legend1);
+            this.chartProdi.Location = new System.Drawing.Point(30, 130);
+            this.chartProdi.Name = "chartProdi";
+            series1.ChartArea = "MainArea";
+            series1.Legend = "MainLegend";
+            series1.Name = "Series1";
+            this.chartProdi.Series.Add(series1);
+            this.chartProdi.Size = new System.Drawing.Size(740, 308);
+            this.chartProdi.TabIndex = 8;
+            this.chartProdi.Text = "chartProdi";
+            // 
+            // FormDashboard
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.ClientSize = new System.Drawing.Size(800, 484);
+            this.Controls.Add(this.chartProdi);
+            this.Controls.Add(this.btnDataMahasiswa);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.cmbTipe);
+            this.Controls.Add(this.lblTipe);
+            this.Controls.Add(this.cmbTahun);
+            this.Controls.Add(this.lblTahun);
+            this.Controls.Add(this.lblJudul);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormDashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard - Rekap Data Mahasiswa";
+            this.Load += new System.EventHandler(this.FormDashboard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartProdi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        // ========== DEKLARASI KOMPONEN ==========
+        private System.Windows.Forms.Label lblJudul;
+        private System.Windows.Forms.Label lblTahun;
+        private System.Windows.Forms.ComboBox cmbTahun;
+        private System.Windows.Forms.Label lblTipe;
+        private System.Windows.Forms.ComboBox cmbTipe;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnDataMahasiswa;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartProdi;
+    }
+}
